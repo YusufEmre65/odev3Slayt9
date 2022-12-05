a1a248b36468f24b1337fccc13a81ddaf95e1efd
Yusuf0665 <95577518+Yusuf0665@users.noreply.github.com>
2022-12-06 02:50:58 +0300

    Add project files.

---
+++ odev3Slayt9/Program.cs
@@ -0,0 +1,37 @@
+﻿using ConsoleApplication9;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ornek1 
+{ 
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            model1 oto1 = new model1(); 
+            model2 oto2 = new model2(); 
+            oto1.tur = "Sedan"; 
+            oto1.silindir_sayisi = 4; 
+            oto1.subap_sayisi = 8; 
+            oto1.guc = 75; 
+            oto1.tork = 100; 
+            oto1.ozellikyaz(); 
+            Console.WriteLine("********"); 
+            oto2.model2_boy = 6; 
+            oto2.model2_agirlik = 900; 
+            oto2.model2_renk = "Kirmizi"; 
+            oto2.ozellikyaz(); 
+            Console.WriteLine("********"); 
+            oto2.goster(); 
+            Console.WriteLine("********"); 
+            oto1.goster(); 
+            Console.ReadLine(); 
+        }
+    }
+}
+
